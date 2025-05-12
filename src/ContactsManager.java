bfd1692ec2298f9ff737ef67e11271a77ab0ad25
brouezechiel <brouezechiel4@gmail.com>
2025-05-12 21:46:21 +0200

    Ajout de l'exercice 1

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,22 +1,37 @@
+// Classe qui gère un carnet de contacts
 public class ContactsManager {
+    // Tableau pour stocker les contacts
     Contact[] myContact;
+    // Compteur pour suivre le nombre de contacts actuels
     int contactCount;
 
+    // Constructeur de la classe
     ContactsManager(){
+        // Initialise le compteur de contacts à 0
         this.contactCount = 0;
+        // Crée un tableau pouvant contenir jusqu'à 500 contacts
         this.myContact = new Contact[500];
     }
 
+    // Méthode pour ajouter un nouveau contact
     void addContact(Contact contact){
+        // Ajoute le contact à la première position disponible
         myContact[contactCount] = contact;
+        // Incrémente le compteur de contacts
         contactCount++;
     }
 
+    // Méthode pour rechercher un contact par son nom
     Contact searchContact(String searchName){
+        // Parcourt tous les contacts existants
         for(int i=0; i<contactCount; i++){
+            // Compare le nom du contact avec le nom recherché
             if(myContact[i].name.equals(searchName)){
+                // Retourne le contact si trouvé
                 return myContact[i];
             }
         }
-        return null;}
+        // Retourne null si le contact n'est pas trouvé
+        return null;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP Pour <b>exécuter</b> le code, appuyez sur <shortcut actionId="Run"/> ou
+// cliquez sur l'icône <icon src="AllIcons.Actions.Execute"/> dans la gouttière.
+
+// Classe principale qui contient la méthode main (point d'entrée du programme)
 public class Main {
     public static void main(String[] args) {
+        // Crée une instance du gestionnaire de contacts
         ContactsManager myContactsManager = new ContactsManager();
 
+        // Crée un premier contact
         Contact myContact = new Contact();
-
+        // Définit le nom et le numéro de téléphone du contact
         myContact.name = "BROU PAUL";
         myContact.phoneNumber = "0574808825";
 
+        // Crée un deuxième contact
         Contact myContact2 = new Contact();
-
+        // Définit le nom et le numéro de téléphone du contact
         myContact2.name = "COULIBALY LAGAYEL";
         myContact2.phoneNumber = "0101622888";
 
+        // Crée un troisième contact
         Contact myContact3 = new Contact();
-
+        // Définit le nom et le numéro de téléphone du contact
         myContact3.name = "SOHOLI BI URIEL";
         myContact3.phoneNumber = "0566167538";
 
+        // Crée un quatrième contact
         Contact myContact4 = new Contact();
-
+        // Définit le nom et le numéro de téléphone du contact
         myContact4.name = "KOUAME ETIENNE";
         myContact4.phoneNumber = "0779527975";
 
+        // Recherche le premier contact par son nom (mais ne stocke pas le résultat)
         myContactsManager.searchContact(myContact.name);
+        // Recherche le premier contact par son numéro (mais la méthode searchContact ne gère pas cette recherche)
         myContactsManager.searchContact(myContact.phoneNumber);
 
+        // Affiche le numéro de téléphone du premier contact
         System.out.println(myContact.phoneNumber);
-        }
     }
+}
