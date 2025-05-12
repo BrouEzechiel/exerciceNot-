e093be4f1699f0c4784e59c4102ff65fc0b571ef
brouezechiel <brouezechiel4@gmail.com>
2025-05-12 21:55:47 +0200

    ajout des commentaires

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,8 @@
+// Classe qui représente un contact dans un carnet d'adresses
 public class Contact {
+    // Champ pour stocker le nom du contact
     String name;
+
+    // Champ pour stocker le numéro de téléphone du contact
     String phoneNumber;
 }
