113038814856643150a13166c676736d76cc7074
brouezechiel <brouezechiel4@gmail.com>
2025-05-14 14:35:25 +0200

    Modification de Main(ajout de de numero et recherche de contact)

--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,38 @@ public class Main {
         // Définit le nom et le numéro de téléphone du contact
         myContact.name = "BROU PAUL";
         myContact.phoneNumber = "0574808825";
+        // Ajoute myContact au tableau
+        myContactsManager.addContact(myContact);
 
         // Crée un deuxième contact
         Contact myContact2 = new Contact();
         // Définit le nom et le numéro de téléphone du contact
         myContact2.name = "COULIBALY LAGAYEL";
         myContact2.phoneNumber = "0101622888";
+        // Ajoute myContact2 au tableau
+        myContactsManager.addContact(myContact2);
 
         // Crée un troisième contact
         Contact myContact3 = new Contact();
         // Définit le nom et le numéro de téléphone du contact
         myContact3.name = "SOHOLI BI URIEL";
         myContact3.phoneNumber = "0566167538";
+        // Ajoute myContact3 au tableau
+        myContactsManager.addContact(myContact3);
 
         // Crée un quatrième contact
         Contact myContact4 = new Contact();
         // Définit le nom et le numéro de téléphone du contact
         myContact4.name = "KOUAME ETIENNE";
         myContact4.phoneNumber = "0779527975";
+        // Ajoute myContact4 au tableau
+        myContactsManager.addContact(myContact4);
 
-        // Recherche le premier contact par son nom (mais ne stocke pas le résultat)
-        myContactsManager.searchContact(myContact.name);
-        // Recherche le premier contact par son numéro (mais la méthode searchContact ne gère pas cette recherche)
-        myContactsManager.searchContact(myContact.phoneNumber);
+        // Recherche d'un contact
+        String searchname = "BROU PAUL";
+        Contact phoneNumber = myContactsManager.searchContact(searchname);
 
         // Affiche le numéro de téléphone du premier contact
-        System.out.println(myContact.phoneNumber);
+        System.out.println("Numero de "+ searchname +" : "+myContact.phoneNumber);
     }
 }
